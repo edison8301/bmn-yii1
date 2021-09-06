1251c4553cab673d4b65947a2303cd907b698eee
Thomas Alfa Edison <thomas.alfa.edison@gmail.com>
2021-09-06 12:24:28 +0700

    Update

--- protected/config/database-sample.php
+++ protected/config/database-sample.php
@@ -2,9 +2,8 @@
 
 // This is the database connection configuration.
 return array(
-	'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
+	//'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
 	// uncomment the following lines to use a MySQL database
-
 	'connectionString' => 'mysql:host=127.0.0.1;dbname=bmn',
 	'emulatePrepare' => true,
 	'username' => 'root',
